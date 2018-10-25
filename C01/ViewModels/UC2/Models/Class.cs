871f1c97090fb9cc55e94f0852f2d515cfc59470
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-10-25 11:54:53 +0200

    Add Use Case 2

---
+++ C01/ViewModels/UC2/Models/Class.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01.ViewModels.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ C01/ViewModels/UC2/ViewModels/StudentFormViewModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01.ViewModels.ViewModels
+{
+    public class CreateStudentViewModel : StudentFormViewModel
+    {
+
+    }
+
+    public class EditStudentViewModel : StudentFormViewModel
+    {
+        public int Id { get; set; }
+        public IEnumerable<string> Classes { get; set; }
+    }
+
+    public class StudentFormViewModel
+    {
+        public string Name { get; set; }
+    }
+}
