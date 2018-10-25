71ecb421e7a689328e4bcecf20752d8482e66eb5
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-10-25 10:05:59 +0200

    Add ViewModels list sample

---
+++ C01/ViewModels/C01.ViewModels/Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+﻿using C01.ViewModels.Services;
+using C01.ViewModels.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01.ViewModels.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly StudentService _studentService = new StudentService();
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            // Get data from the data store
+            var students = await _studentService.ReadAllAsync();
+
+            // Create the ViewModel, based on the data
+            var viewModel = new StudentListViewModel
+            {
+                Students = students.Select(student => new StudentListItemViewModel
+                {
+                    Id = student.Id,
+                    Name = student.Name,
+                    ClassCount = student.Classes.Count()
+                })
+            };
+
+            // Return the View
+            return View(viewModel);
+        }
+    }
+}
---
+++ C01/ViewModels/C01.ViewModels/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace C01.ViewModels.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
