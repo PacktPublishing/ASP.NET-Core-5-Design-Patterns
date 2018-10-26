68d4cc5d0659fa3365f6bf6ff81c54f2ee6b968f
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-10-26 14:36:09 +0200

    Create Initial project

---
+++ C02/Single Responsibility/1-Initial Program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C02.SRP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
