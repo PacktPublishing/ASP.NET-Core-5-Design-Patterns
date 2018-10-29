97b0164a82585833f9aa16717cb16aaed28391e1
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-10-29 15:41:03 +0100

    Add models, DTOs and Controller

---
+++ C01/WebApi/UC1/Controllers/ClientsController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using C01.WebApi.Models;
+using C01.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace C01.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        private readonly ClientService _clientService = new ClientService();
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<ClientDto>> Get()
+        {
+            var clients = _clientService.ReadAll();
+            return clients.Select(client => Convert(client)).ToArray();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var client = _clientService.ReadOne(id);
+            if (client == default(Client))
+            {
+                return NotFound();
+            }
+            var dto = Convert(client);
+            return Ok(dto);
+        }
+
+        private ClientDto Convert(Client client)
+        {
+            return new ClientDto
+            {
+                Id = client.Id,
+                Name = client.Name,
+                Contracts = client.Contracts.Select(contract => new ContractDto
+                {
+                    Id = contract.Id,
+                    Name = contract.Name,
+                    Description = contract.Description,
+
+                    // Flattening PrimaryContact
+                    PrimaryContactEmail = contract.PrimaryContact.Email,
+                    PrimaryContactFirstname = contract.PrimaryContact.Firstname,
+                    PrimaryContactLastname = contract.PrimaryContact.Lastname,
+
+                    // Flattening Work
+                    WorkDone = contract.Work.Done,
+                    WorkState = contract.Work.State,
+                    WorkTotal = contract.Work.Total
+                })
+            };
+        }
+    }
+}
---
+++ C01/WebApi/UC1/Models/Client.cs
@@ -0,0 +1,89 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C01.WebApi.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Contract> Contracts { get; set; }
+    }
+
+    public class Contract
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public ContractWork Work { get; set; }
+        public ContactInformation PrimaryContact { get; set; }
+    }
+
+    public class ContractWork
+    {
+        public int Total { get; set; }
+        public int Done { get; set; }
+
+        public WorkState State => 
+            Done == 0 ? WorkState.New : 
+            Done == Total ? WorkState.Completed : 
+            WorkState.InProgress;
+    }
+
+    public enum WorkState
+    {
+        New,
+        InProgress,
+        Completed
+    }
+
+    public class ContactInformation
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ClientDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("contracts")]
+        public IEnumerable<ContractDto> Contracts { get; set; }
+    }
+
+    public class ContractDto
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+        [JsonProperty("name")]
+        public string Name { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("workTotal")]
+        public int WorkTotal { get; set; }
+        [JsonProperty("workDone")]
+        public int WorkDone { get; set; }
+        [JsonProperty("workState")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public WorkState WorkState { get; set; }
+
+        [JsonProperty("primaryContactFirstname")]
+        public string PrimaryContactFirstname { get; set; }
+        [JsonProperty("primaryContactLastname")]
+        public string PrimaryContactLastname { get; set; }
+        [JsonProperty("primaryContactEmail")]
+        public string PrimaryContactEmail { get; set; }
+    }
+}
