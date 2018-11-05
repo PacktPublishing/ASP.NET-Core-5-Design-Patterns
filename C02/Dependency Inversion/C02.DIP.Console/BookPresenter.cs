fff2aa792bbae03a518275efbc05f7eab2584a0e
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-11-05 16:14:09 +0100

    Add Dependency Inversion Principle use case 1

---
+++ C02/Dependency Inversion/C02.DIP.Console/BookPresenter.cs
@@ -0,0 +1,17 @@
+﻿using C02.DIP.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C02.DIP.App
+{
+    public class BookPresenter
+    {
+        public void Display(Book book)
+        {
+            Console.WriteLine($"Book: {book.Title} ({book.Id})");
+        }
+    }
+}
