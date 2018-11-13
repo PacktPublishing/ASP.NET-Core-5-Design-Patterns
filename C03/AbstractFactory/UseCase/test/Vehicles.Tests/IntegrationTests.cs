2d200959b3b37f17c27b2e8c30e08c2a1525a22b
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-11-13 14:26:55 +0100

    Move AbstractFactory to UseCase and duplicate the solution to Expansion

---
+++ C03/AbstractFactory/UseCase/test/Vehicles.Tests/IntegrationTests.cs
@@ -0,0 +1,44 @@
+using System;
+using Vehicles.Models;
+using Xunit;
+
+namespace Vehicles.Tests
+{
+    public class AbstractFactory
+    {
+        // Arrange
+        public static IVehicleFactory LowGradeVehicleFactory => new LowGradeVehicleFactory();
+        public static IVehicleFactory HighGradeVehicleFactory => new HighGradeVehicleFactory();
+
+        public static TheoryData<IVehicleFactory, Type> CarData => new TheoryData<IVehicleFactory, Type>{
+            { LowGradeVehicleFactory, typeof(LowGradeCar) },
+            { HighGradeVehicleFactory, typeof(HighGradeCar) },
+        };
+        public static TheoryData<IVehicleFactory, Type> BikeData => new TheoryData<IVehicleFactory, Type>{
+            { LowGradeVehicleFactory, typeof(LowGradeBike) },
+            { HighGradeVehicleFactory, typeof(HighGradeBike) },
+        };
+
+        [Theory]
+        [MemberData(nameof(CarData))]
+        public void Should_create_a_Car_of_the_specified_type(IVehicleFactory vehicleFactory, Type expectedCarType)
+        {
+            // Act
+            var result = vehicleFactory.CreateCar();
+
+            // Assert
+            Assert.IsType(expectedCarType, result);
+        }
+
+        [Theory]
+        [MemberData(nameof(BikeData))]
+        public void Should_create_a_Bike_of_the_specified_type(IVehicleFactory vehicleFactory, Type expectedBikeType)
+        {
+            // Act
+            var result = vehicleFactory.CreateBike();
+
+            // Assert
+            Assert.IsType(expectedBikeType, result);
+        }
+    }
+}
