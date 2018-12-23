fe892c0a79015ab7f66d3d0b7df5e69be9390c58
Carl-Hugo Marcotte <carlhugo@gmail.com>
2018-12-23 15:57:50 -0500

    Duplicate Strategy into Factory

---
+++ C04/Factory/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using Factory.Services;
+
+namespace Factory.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IHomeService _homeService;
+        public HomeController(IHomeService homeService)
+        {
+            _homeService = homeService ?? throw new ArgumentNullException(nameof(homeService));
+        }
+
+        //public IActionResult Index([FromServices]HomePageViewModel viewModel)
+        public IActionResult Index()
+        {
+            var data = _homeService.GetHomePageData();
+            var viewModel = new HomePageViewModel(data);
+            return View(viewModel);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
