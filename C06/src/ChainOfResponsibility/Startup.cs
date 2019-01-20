c123cefb428c3cbbe7f0c7b7dba35143d05c628c
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-01-19 20:32:02 -0500

    Remove template comments

--- C06/src/ChainOfResponsibility/Startup.cs
+++ C06/src/ChainOfResponsibility/Startup.cs
@@ -11,14 +11,11 @@ namespace ChainOfResponsibility
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IMessageHandler>(new AlarmTriggeredHandler(new AlarmPausedHandler(new AlarmStoppedHandler(new DefaultHandler()))));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageHandler messageHandler)
         {
             if (env.IsDevelopment())
