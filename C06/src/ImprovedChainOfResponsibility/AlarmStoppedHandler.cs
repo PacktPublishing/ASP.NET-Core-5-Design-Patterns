aff522946dc27b9d5d41176086325a5f25af3163
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-01-19 20:38:49 -0500

    Add the ImprovedChainOfResponsibility project

---
+++ C06/src/ImprovedChainOfResponsibility/AlarmStoppedHandler.cs
@@ -0,0 +1,23 @@
+﻿namespace ImprovedChainOfResponsibility
+{
+    public class AlarmStoppedHandler : IMessageHandler
+    {
+        private readonly IMessageHandler _next;
+        public AlarmStoppedHandler(IMessageHandler next = null)
+        {
+            _next = next;
+        }
+
+        public void Handle(Message message)
+        {
+            if (message.Name == "AlarmStopped")
+            {
+                // Do something cleaver with the Payload
+            }
+            else if (_next != null)
+            {
+                _next.Handle(message);
+            }
+        }
+    }
+}
---
+++ C06/src/ImprovedChainOfResponsibility/Startup.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ImprovedChainOfResponsibility
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMessageHandler>(new AlarmTriggeredHandler(new AlarmPausedHandler(new AlarmStoppedHandler(new DefaultHandler()))));
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMessageHandler messageHandler)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                var message = new Message
+                {
+                    Name = context.Request.Query["name"],
+                    Payload = context.Request.Query["payload"]
+                };
+                try
+                {
+                    messageHandler.Handle(message);
+                    await context.Response.WriteAsync($"Message '{message.Name}' handled successfully.");
+                }
+                catch (NotSupportedException ex)
+                {
+                    await context.Response.WriteAsync(ex.Message);
+                }
+            });
+        }
+    }
+}
