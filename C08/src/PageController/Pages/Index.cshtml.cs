41fc30eb542e72e54658760552cac66304bf08ec
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-02-13 12:11:50 -0500

    Initial project

---
+++ C08/src/PageController/Pages/Index.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PageController.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public string Toto { get; set; }
+
+        public void OnGet(string toto)
+        {
+            Toto = toto;
+        }
+    }
+}
