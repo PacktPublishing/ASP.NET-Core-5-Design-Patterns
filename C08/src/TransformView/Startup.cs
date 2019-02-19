fe0d3e5b3d3cdf1fe3ee699ddc2ad0782ee8470e
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-02-19 09:53:03 -0500

    Add Employee CRUD module to TransformView

--- C08/src/TransformView/Startup.cs
+++ C08/src/TransformView/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TransformView.Data;
 
 namespace TransformView
 {
@@ -30,7 +32,9 @@ namespace TransformView
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
-
+            services.AddDbContext<EmployeeDbContext>(
+                options => options.UseInMemoryDatabase(nameof(EmployeeDbContext))
+            );
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
