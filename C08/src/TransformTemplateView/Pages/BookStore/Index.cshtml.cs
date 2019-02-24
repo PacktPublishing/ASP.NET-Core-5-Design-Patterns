cc716d57f50f818f139f95c7bd26eae4c4747c75
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-02-24 16:19:53 -0500

    Clean BookStore/Index

--- C08/src/TransformTemplateView/Pages/BookStore/Index.cshtml.cs
+++ C08/src/TransformTemplateView/Pages/BookStore/Index.cshtml.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
 using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using TransformTemplateView.Models;
 using TransformTemplateView.Services;
 
