04993b3e7c39a84a2b29b9b34b860d038c446c74
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-05-20 12:37:57 -0400

    Add the Item class

--- C10/src/DomainModel/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DomainModel
-{
-    public class Class1
-    {
-    }
-}
---
+++ C10/src/DomainModel/Item.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DomainModel
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int QuantityInStock { get; set; }
+    }
+}
