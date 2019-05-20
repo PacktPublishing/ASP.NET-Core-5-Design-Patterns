b5e12e3b46386dfc514318e129f241f1a8da3b65
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-05-20 13:31:04 -0400

    Add Item operations

--- C10/src/DomainModel/Item.cs
+++ C10/src/DomainModel/Item.cs
@@ -1,11 +1,23 @@
-﻿using System;
-
-namespace DomainModel
+﻿namespace DomainModel
 {
     public class Item
     {
         public int Id { get; set; }
         public string Name { get; set; }
         public int QuantityInStock { get; set; }
+
+        public void AddStock(int amount)
+        {
+            QuantityInStock += amount;
+        }
+
+        public void RemoveStock(int amount)
+        {
+            if (amount > QuantityInStock)
+            {
+                throw new NotEnoughStockException(QuantityInStock, amount);
+            }
+            QuantityInStock -= amount;
+        }
     }
 }
