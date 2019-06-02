88649e7837301bcdfaa1084dd623acea1ce9394c
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-06-02 14:15:51 -0400

    Add client code to try the system out

--- C04/ApplicationState/Startup.cs
+++ C04/ApplicationState/Startup.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -19,7 +20,7 @@ namespace ApplicationState
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IMyApplicationWideService myAppState)
         {
             if (env.IsDevelopment())
             {
@@ -28,8 +29,40 @@ namespace ApplicationState
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                if (context.Request.Method == "GET")
+                {
+                    await HandleGetRequestAsync(myAppState, context);
+                }
+                else
+                {
+                    await HandlePostRequestAsync(myAppState, context);
+                }
             });
+        }
+
+        private static async Task HandleGetRequestAsync(IMyApplicationWideService myAppState, HttpContext context)
+        {
+            var key = context.Request.Query["key"];
+            if (key.Count != 1)
+            {
+                await context.Response.WriteAsync("You must specify a single 'key' parameter like '?key=SomeAppStateKey'.");
+                return;
+            }
+            var value = myAppState.Get<string>(key.Single());
+            await context.Response.WriteAsync($"{key} = {value ?? "null"}");
+        }
+
+        private async Task HandlePostRequestAsync(IMyApplicationWideService myAppState, HttpContext context)
+        {
+            var key = context.Request.Form["key"].SingleOrDefault();
+            var value = context.Request.Form["value"].SingleOrDefault();
+            if (key == null || value == null)
+            {
+                await context.Response.WriteAsync("You must specify both a 'key' and a 'value'.");
+                return;
+            }
+            myAppState.Set(key, value);
+            await context.Response.WriteAsync($"{key} = {value ?? "null"}");
         }
     }
 }
