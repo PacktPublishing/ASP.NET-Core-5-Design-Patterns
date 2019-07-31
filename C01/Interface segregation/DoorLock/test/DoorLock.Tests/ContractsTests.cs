fbf1f32f1c2669b0d4882487941035aadc6f9129
Carl-Hugo Marcotte <carlhugo@gmail.com>
2019-07-31 00:04:38 -0400

    Add some contract tests

---
+++ C01/Interface segregation/DoorLock/test/DoorLock.Tests/ContractsTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DoorLock
+{
+    public class ContractsTests
+    {
+        [Fact]
+        public void A_single_key_should_fit_identical_locks()
+        {
+            var key = new BasicKey("key1");
+
+            LockAndAssertResult(new BasicLock("key1"));
+            LockAndAssertResult(new BasicLock("key1"));
+            LockAndAssertResult(new MultiLock(new List<ILock> {
+                new BasicLock("key1"),
+                new BasicLock("key1")
+            }));
+
+            void LockAndAssertResult(ILock @lock)
+            {
+                var result = @lock.DoesMatch(key);
+                Assert.True(result, $"The key '{key.Signature}' should fit the lock");
+            }
+        }
+
+        [Fact]
+        public void Multiple_keys_should_fit_the_same_lock()
+        {
+            var @lock = new BasicLock("key1");
+
+            LockAndAssertResult(new BasicKey("key1"));
+            LockAndAssertResult(new BasicKey("key1"));
+            LockAndAssertResult(new BasicKey("key1"));
+
+            void LockAndAssertResult(IKey key)
+            {
+                var result = @lock.DoesMatch(key);
+                Assert.True(result, $"The key '{key.Signature}' should fit the lock");
+            }
+        }
+    }
+}
