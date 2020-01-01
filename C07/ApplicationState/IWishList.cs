2b595109619a524b184ef4ae7f4e79c4341d3827
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-01-01 14:48:10 -0500

    Extract classes to files

---
+++ C07/ApplicationState/IWishList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApplicationState
+{
+    public interface IWishList
+    {
+        Task<WishListItem> AddOrRefreshAsync(string itemName);
+        Task<IEnumerable<WishListItem>> AllAsync();
+    }
+}
--- C07/ApplicationState/WishListController.cs
+++ C07/ApplicationState/WishListController.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Caching.Memory;
-using Microsoft.Extensions.Options;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -32,92 +29,6 @@ namespace ApplicationState
         {
             var result = await _wishList.AddOrRefreshAsync(itemName);
             return CreatedAtAction(nameof(GetAsync), result);
-        }
-    }
-
-    public class WishListItem
-    {
-        public int Count { get; set; }
-        public DateTimeOffset Expiration { get; set; }
-        public string Name { get; set; }
-    }
-
-    public interface IWishList
-    {
-        Task<WishListItem> AddOrRefreshAsync(string itemName);
-        Task<IEnumerable<WishListItem>> AllAsync();
-    }
-
-    public class InMemoryWishList : IWishList
-    {
-        private readonly InMemoryWishListOptions _options;
-        private readonly Dictionary<string, InternalItem> _items;
-
-        public InMemoryWishList(IOptions<InMemoryWishListOptions> optionsAccessor)
-        {
-            _options = optionsAccessor.Value;
-            _items = new Dictionary<string, InternalItem>();
-        }
-
-        public Task<WishListItem> AddOrRefreshAsync(string itemName)
-        {
-            var expirationTime = _options.SystemClock.UtcNow.AddSeconds(_options.ExpirationInSeconds);
-            _items
-                .Where(x => x.Value.Expiration < _options.SystemClock.UtcNow)
-                .Select(x => x.Key)
-                .ToList()
-                .ForEach(key => _items.Remove(key))
-            ;
-            if (_items.ContainsKey(itemName))
-            {
-                var item = _items[itemName];
-                item.Count++;
-                item.Expiration = expirationTime;
-                var wishlistItem = new WishListItem
-                {
-                    Name = itemName,
-                    Count = item.Count,
-                    Expiration = item.Expiration
-                };
-                return Task.FromResult(wishlistItem);
-            }
-            else
-            {
-                var item = new InternalItem
-                {
-                    Count = 1,
-                    Expiration = expirationTime
-                };
-                _items.Add(itemName, item);
-                var wishlistItem = new WishListItem
-                {
-                    Name = itemName,
-                    Count = item.Count,
-                    Expiration = item.Expiration
-                };
-                return Task.FromResult(wishlistItem);
-            }
-        }
-
-        public Task<IEnumerable<WishListItem>> AllAsync()
-        {
-            var items = _items
-                .Select(x => new WishListItem
-                {
-                    Name = x.Key,
-                    Count = x.Value.Count,
-                    Expiration = x.Value.Expiration
-                })
-                .OrderByDescending(x => x.Count)
-                .AsEnumerable()
-            ;
-            return Task.FromResult(items);
-        }
-
-        private class InternalItem
-        {
-            public int Count { get; set; }
-            public DateTimeOffset Expiration { get; set; }
         }
     }
 }
