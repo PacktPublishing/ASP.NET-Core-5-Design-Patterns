bfb0e59b468de44d3e951b40e759a0fbf6ad8292
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-01-10 15:58:51 -0500

    Add Return Values

---
+++ C05/Return Values/ReturnValueController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Return_Values
+{
+    [Route("/")]
+    public class ReturnValueController : ControllerBase
+    {
+        [HttpGet("ActionResultMyResult/{input}")]
+        public ActionResult<MyResult> GetActionResultMyResult(int input)
+        {
+            return Ok(new MyResult
+            {
+                Input = input,
+                Value = "GetActionResultMyResult"
+            });
+        }
+
+        [HttpGet("ActionResultMyResult/{input}")]
+        public ActionResult<MyResult> GetMyResult(int input)
+        {
+            return new MyResult
+            {
+                Input = input,
+                Value = "GetActionResultMyResult"
+            };
+        }
+
+        [HttpGet("ActionResultMyResult/{input}")]
+        public IActionResult GetIActionResult(int input)
+        {
+            return Ok(new MyResult
+            {
+                Input = input,
+                Value = "GetActionResultMyResult"
+            });
+        }
+    }
+}
