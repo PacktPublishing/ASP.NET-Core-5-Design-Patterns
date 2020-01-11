749ed4a110285c830b28f4ad6ccac564d9e99ea3
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-01-11 16:53:49 -0500

    Update ReturnValueController

--- C05/Return Values/ReturnValueController.cs
+++ C05/Return Values/ReturnValueController.cs
@@ -1,38 +1,57 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace Return_Values
 {
     [Route("/")]
     public class ReturnValueController : ControllerBase
     {
+        [HttpGet]
+        public IEnumerable<string> Index()
+        {
+            yield return AbsoluteUriOfAction(nameof(GetActionResultMyResult));
+            yield return AbsoluteUriOfAction(nameof(GetMyResult));
+            yield return AbsoluteUriOfAction(nameof(GetIActionResult));
+        }
+
         [HttpGet("ActionResultMyResult/{input}")]
         public ActionResult<MyResult> GetActionResultMyResult(int input)
         {
             return Ok(new MyResult
             {
                 Input = input,
-                Value = "GetActionResultMyResult"
+                Value = nameof(GetActionResultMyResult)
             });
         }
 
-        [HttpGet("ActionResultMyResult/{input}")]
+        [HttpGet("GetMyResult/{input}")]
         public ActionResult<MyResult> GetMyResult(int input)
         {
             return new MyResult
             {
                 Input = input,
-                Value = "GetActionResultMyResult"
+                Value = nameof(GetMyResult)
             };
         }
 
-        [HttpGet("ActionResultMyResult/{input}")]
+        [HttpGet("GetIActionResult/{input}")]
         public IActionResult GetIActionResult(int input)
         {
             return Ok(new MyResult
             {
                 Input = input,
-                Value = "GetActionResultMyResult"
+                Value = nameof(GetIActionResult)
             });
+        }
+
+        private string AbsoluteUriOfAction(string action)
+        {
+            return Url.Action(
+                action,
+                ControllerContext.ActionDescriptor.ControllerName,
+                new { input = 1 },
+                HttpContext.Request.Scheme
+            );
         }
     }
 }
