1ec62b7a580ebb9d6862d23fbb16e0fbee79c020
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-02-24 15:27:54 -0500

    Add options framework exploration tests

---
+++ C08/CommonScenarios.Tests/DefaultOptionsTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommonScenarios
+{
+    public class DefaultOptionsTests
+    {
+        public const string DefaultOptionName = "Default Options";
+        private IServiceProvider _serviceProvider;
+
+        public DefaultOptionsTests()
+        {
+            var services = new ServiceCollection();
+            services.AddOptions();
+            services.Configure<MyOptions>(myOptions =>
+            {
+                myOptions.Name = DefaultOptionName;
+            });
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void OptionsSnapshot()
+        {
+            var snapshot = _serviceProvider.GetRequiredService<IOptionsSnapshot<MyOptions>>();
+            Assert.NotNull(snapshot.Value);
+            Assert.Equal(DefaultOptionName, snapshot.Value.Name);
+        }
+
+        [Fact]
+        public void OptionsMonitor()
+        {
+            var monitor = _serviceProvider.GetRequiredService<IOptionsMonitor<MyOptions>>();
+            Assert.NotNull(monitor.CurrentValue);
+            Assert.Equal(DefaultOptionName, monitor.CurrentValue.Name);
+        }
+    }
+}
---
+++ C08/CommonScenarios.Tests/EmptyDefaultOptionsTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CommonScenarios
+{
+    public class EmptyDefaultOptionsTests
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public EmptyDefaultOptionsTests()
+        {
+            var services = new ServiceCollection();
+            services.AddOptions();
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void OptionsSnapshot()
+        {
+            var snapshot = _serviceProvider.GetRequiredService<IOptionsSnapshot<MyOptions>>();
+            Assert.NotNull(snapshot.Value);
+        }
+
+        [Fact]
+        public void OptionsMonitor()
+        {
+            var monitor = _serviceProvider.GetRequiredService<IOptionsMonitor<MyOptions>>();
+            Assert.NotNull(monitor.CurrentValue);
+        }
+
+    }
+}
