4b8fcf1d85788deb8028a68861658fbe5e6f84dd
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-04-23 15:20:22 -0400

    Code ConfigureMeWithDataAnnotations tests

--- C08/OptionsDataAnnotations/ConfigureMeWithDataAnnotations.cs
+++ C08/OptionsDataAnnotations/ConfigureMeWithDataAnnotations.cs
@@ -1,5 +1,7 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Xunit;
 
 namespace OptionsDataAnnotations
@@ -7,15 +9,35 @@ namespace OptionsDataAnnotations
     public class ConfigureMeWithDataAnnotations
     {
         [Fact]
-        public void Test1()
+        public void Should_pass_validation()
         {
+            var services = new ServiceCollection();
+            services.AddOptions<Options>()
+                .Configure(o => o.MyImportantProperty = "Some important value")
+                .ValidateDataAnnotations();
+            var serviceProvider = services.BuildServiceProvider();
+            var options = serviceProvider.GetService<IOptionsMonitor<Options>>();
+            Assert.Equal("Some important value", options.CurrentValue.MyImportantProperty);
+        }
 
+        [Fact]
+        public void Should_fail_validation()
+        {
+            var services = new ServiceCollection();
+            services.AddOptions<Options>()
+                .ValidateDataAnnotations();
+            var serviceProvider = services.BuildServiceProvider();
+            var error = Assert.Throws<OptionsValidationException>(()
+                => serviceProvider.GetService<IOptionsMonitor<Options>>().CurrentValue);
+            Assert.Collection(error.Failures,
+                f => Assert.Equal("DataAnnotation validation failed for members: 'MyImportantProperty' with the error: 'The MyImportantProperty field is required.'.", f)
+            ); ;
         }
 
         private class Options
         {
             [Required]
-            public string Title { get; set; }
+            public string MyImportantProperty { get; set; }
         }
     }
 }
