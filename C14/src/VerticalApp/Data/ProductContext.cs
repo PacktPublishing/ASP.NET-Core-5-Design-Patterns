f4c3e1d295ef7fd5e3c5d86126a69a7ff01470c6
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-05-19 14:47:49 -0400

    Add the 3 use cases

---
+++ C14/src/VerticalApp/Data/ProductContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VerticalApp.Data
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options)
+            : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- C14/src/VerticalApp/Startup.cs
+++ C14/src/VerticalApp/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AutoMapper;
+using MediatR;
+using FluentValidation.AspNetCore;
 
 namespace VerticalApp
 {
@@ -16,6 +19,15 @@ namespace VerticalApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var currentAssembly = GetType().Assembly;
+            services.AddAutoMapper(currentAssembly);
+            services.AddMediatR(currentAssembly);
+            services
+                .ScanForDIModules()
+                .FromAssemblyOf<Startup>();
+            services
+                .AddControllers()
+                .AddFluentValidation(config => config.RegisterValidatorsFromAssembly(currentAssembly));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,16 +37,16 @@ namespace VerticalApp
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseHttpsRedirection();
             app.UseRouting();
-
+            app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
+
+            // Seed temp data
+            app.Seed<Data.ProductContext>();
         }
     }
 }
