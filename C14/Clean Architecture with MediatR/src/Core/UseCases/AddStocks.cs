93c9d494ec9eba35a7a903c5626349b11bb72822
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-05-22 01:08:21 -0400

    Add Clean Architecture with MediatR

---
+++ C14/Clean Architecture with MediatR/src/Core/UseCases/AddStocks.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.UseCases
+{
+    public class AddStocks
+    {
+        public class Command : IRequest<Product>
+        {
+            public int ProductId { get; set; }
+            public int Amount { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Product>
+        {
+            private readonly IProductRepository _productRepository;
+            public Handler(IProductRepository productRepository)
+            {
+                _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            }
+
+            public Task<Product> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var product = _productRepository.FindById(request.ProductId);
+                product.QuantityInStock += request.Amount;
+                _productRepository.Update(product);
+                return Task.FromResult(product);
+            }
+        }
+    }
+}
---
+++ C14/Clean Architecture with MediatR/src/Core/UseCases/RemoveStocks.cs
@@ -0,0 +1,42 @@
+﻿using Core.Entities;
+using Core.Interfaces;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Core.UseCases
+{
+    public class RemoveStocks
+    {
+        public class Command : IRequest<Product>
+        {
+            public int ProductId { get; set; }
+            public int Amount { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, Product>
+        {
+            private readonly IProductRepository _productRepository;
+            public Handler(IProductRepository productRepository)
+            {
+                _productRepository = productRepository ?? throw new ArgumentNullException(nameof(productRepository));
+            }
+
+            public Task<Product> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var product = _productRepository.FindById(request.ProductId);
+                if (request.Amount > product.QuantityInStock)
+                {
+                    throw new NotEnoughStockException(product.QuantityInStock, request.Amount);
+                }
+                product.QuantityInStock -= request.Amount;
+                _productRepository.Update(product);
+                return Task.FromResult(product);
+            }
+        }
+    }
+}
