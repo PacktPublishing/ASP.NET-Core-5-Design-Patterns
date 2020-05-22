6024f84ac1e6ba3c7e93a4ec5bdb9579760c818a
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-05-22 00:21:07 -0400

    Refactor tests

--- C14/Mediator/test/Mediator.Tests/MediatorTest.cs
+++ C14/Mediator/test/Mediator.Tests/MediatorTest.cs
@@ -6,32 +6,23 @@ using Microsoft.Extensions.Logging;
 using ForEvolve.Testing.Logging;
 using Moq;
 using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
 
 namespace Mediator.Tests
 {
     public class MediatorTest
     {
-        private readonly List<string> _lines = new List<string>();
-        private readonly ILoggerFactory _loggerFactory;
-        public MediatorTest(ITestOutputHelper output)
-        {
-            if (output == null) { throw new ArgumentNullException(nameof(output)); }
-            //ILoggerProvider loggerProvider = new XunitTestOutputLoggerProvider(output);
-            ILoggerProvider loggerProvider = new AssertableLoggerProvider(_lines);
-            var loggerFactoryMock = new Mock<ILoggerFactory>();
-            loggerFactoryMock
-                .Setup(x => x.CreateLogger(It.IsAny<string>()))
-                .Returns((string categoryName) => loggerProvider.CreateLogger(categoryName));
-            _loggerFactory = loggerFactoryMock.Object;
-        }
-
         [Fact]
         public void Send_a_message_to_all_colleagues()
         {
             // Arrange
-            var miller = new ConcreteColleague("Miller", _loggerFactory);
-            var orazio = new ConcreteColleague("Orazio", _loggerFactory);
-            var fletcher = new ConcreteColleague("Fletcher", _loggerFactory);
+            var lines = new List<string>();
+            var loggerFactory = CreateLoggerFactory(lines);
+
+            var miller = new ConcreteColleague("Miller", loggerFactory);
+            var orazio = new ConcreteColleague("Orazio", loggerFactory);
+            var fletcher = new ConcreteColleague("Fletcher", loggerFactory);
 
             var mediator = new BroadcastMediator(miller, orazio, fletcher);
 
@@ -50,7 +41,7 @@ namespace Mediator.Tests
             ));
 
             // Assert
-            Assert.Collection(_lines,
+            Assert.Collection(lines,
                 line => Assert.Equal("[from: Miller][to: Miller]: Hey everyone!", line),
                 line => Assert.Equal("[from: Miller][to: Orazio]: Hey everyone!", line),
                 line => Assert.Equal("[from: Miller][to: Fletcher]: Hey everyone!", line),
@@ -69,9 +60,12 @@ namespace Mediator.Tests
         public void Send_a_direct_message()
         {
             // Arrange
-            var miller = new ConcreteColleague("Miller", _loggerFactory);
-            var orazio = new ConcreteColleague("Orazio", _loggerFactory);
-            var fletcher = new ConcreteColleague("Fletcher", _loggerFactory);
+            var lines = new List<string>();
+            var loggerFactory = CreateLoggerFactory(lines);
+
+            var miller = new ConcreteColleague("Miller", loggerFactory);
+            var orazio = new ConcreteColleague("Orazio", loggerFactory);
+            var fletcher = new ConcreteColleague("Fletcher", loggerFactory);
 
             var mediator = new DirectMessageMediator(miller, orazio, fletcher);
 
@@ -90,8 +84,7 @@ namespace Mediator.Tests
             ));
 
             // Assert
-            // Assert
-            Assert.Collection(_lines,
+            Assert.Collection(lines,
                 line => Assert.Equal("[from: Miller][to: Miller]: Hey Orazio!", line),
                 line => Assert.Equal("[from: Miller][to: Orazio]: Hey Orazio!", line),
                 line => Assert.Equal("[from: Orazio][to: Miller]: What's up Miller?", line),
@@ -99,6 +92,62 @@ namespace Mediator.Tests
                 line => Assert.Equal("[from: Miller][to: Miller]: Great Orazio, and you?", line),
                 line => Assert.Equal("[from: Miller][to: Orazio]: Great Orazio, and you?", line)
             );
+        }
+
+        [Fact]
+        public void Send_a_direct_message_using_dependency_injection()
+        {
+            // Arrange
+            var lines = new List<string>();
+            var loggerFactory = CreateLoggerFactory(lines);
+
+            var miller = new ConcreteColleague("Miller", loggerFactory);
+            var orazio = new ConcreteColleague("Orazio", loggerFactory);
+            var fletcher = new ConcreteColleague("Fletcher", loggerFactory);
+
+            var services = new ServiceCollection();
+            services.AddSingleton<IMediator, DirectMessageMediator>();
+            services.AddSingleton(serviceProvider => serviceProvider.GetServices<IColleague>().ToArray());
+            services.AddSingleton<IColleague>(miller);
+            services.AddSingleton<IColleague>(orazio);
+            services.AddSingleton<IColleague>(fletcher);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var mediator = serviceProvider.GetRequiredService<IMediator>();
+
+            // Act
+            mediator.Send(new Message(
+                from: miller,
+                content: "Hey Orazio!"
+            ));
+            mediator.Send(new Message(
+                from: orazio,
+                content: "What's up Miller?"
+            ));
+            mediator.Send(new Message(
+                from: miller,
+                content: "Great Orazio, and you?"
+            ));
+
+            // Assert
+            Assert.Collection(lines,
+                line => Assert.Equal("[from: Miller][to: Miller]: Hey Orazio!", line),
+                line => Assert.Equal("[from: Miller][to: Orazio]: Hey Orazio!", line),
+                line => Assert.Equal("[from: Orazio][to: Miller]: What's up Miller?", line),
+                line => Assert.Equal("[from: Orazio][to: Orazio]: What's up Miller?", line),
+                line => Assert.Equal("[from: Miller][to: Miller]: Great Orazio, and you?", line),
+                line => Assert.Equal("[from: Miller][to: Orazio]: Great Orazio, and you?", line)
+            );
+        }
+
+        private ILoggerFactory CreateLoggerFactory(IList<string> lines)
+        {
+            ILoggerProvider loggerProvider = new AssertableLoggerProvider(lines);
+            var loggerFactoryMock = new Mock<ILoggerFactory>();
+            loggerFactoryMock
+                .Setup(x => x.CreateLogger(It.IsAny<string>()))
+                .Returns((string categoryName) => loggerProvider.CreateLogger(categoryName));
+            return loggerFactoryMock.Object;
         }
     }
 }
