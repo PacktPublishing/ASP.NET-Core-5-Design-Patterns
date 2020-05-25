8e2868a8dbd4faefa48929c1d2182c5fb4621ea6
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-05-25 15:13:48 -0400

    Add usage cases of the Mediator

--- C14/CQRS/test/CQRS.Tests/ChatRoomTest.cs
+++ C14/CQRS/test/CQRS.Tests/ChatRoomTest.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using Xunit;
 
@@ -7,7 +9,7 @@ namespace CQRS.Tests
 {
     public class ChatRoomTest
     {
-        //private readonly IMediator _mediator = default;
+        private readonly IMediator _mediator = new Mediator();
         private readonly TestMessageWriter _reagenMessageWriter = new TestMessageWriter();
         private readonly TestMessageWriter _garnerMessageWriter = new TestMessageWriter();
         private readonly TestMessageWriter _corneliaMessageWriter = new TestMessageWriter();
@@ -21,7 +23,6 @@ namespace CQRS.Tests
 
         public ChatRoomTest()
         {
-            var _mediator = new Mediator();
             _mediator.Register(new JoinChatRoom.Handler());
             _mediator.Register(new LeaveChatRoom.Handler());
             _mediator.Register(new SendChatMessage.Handler());
@@ -117,6 +118,94 @@ namespace CQRS.Tests
             );
         }
 
+        [Fact]
+        public void Another_component_could_send_commands_using_the_mediator()
+        {
+            _reagen.Join(_room1);
+            _garner.Join(_room1);
+            _garner.Join(_room2);
+            _mediator.Send(new SendChatMessage.Command(_room1, new ChatMessage(_reagen, "Hello!")));
+
+            Assert.Collection(_garnerMessageWriter.Output,
+                line =>
+                {
+                    Assert.Equal(_room1, line.chatRoom);
+                    Assert.Equal(_reagen, line.message.Sender);
+                    Assert.Equal("Hello!", line.message.Message);
+                }
+            );
+        }
+
+        [Fact]
+        public void Another_component_could_react_to_commands()
+        {
+            var moderatorChatRoom = new ChatRoom("Moderator Room");
+            var moderator = new ChatModerator(_mediator, moderatorChatRoom, new string[] { "bad" });
+            _mediator.Register(moderator);
+
+            _reagen.Join(_room1);
+            _garner.Join(_room1);
+            _garner.Join(_room2);
+            _reagen.SendMessageTo(_room1, "Hello bad!");
+            _garner.SendMessageTo(_room2, "bad Hello!");
+
+            var messages = moderatorChatRoom.ListMessages();
+            Assert.Collection(messages,
+                message =>
+                {
+                    Assert.Equal(moderator, message.Sender);
+                    Assert.StartsWith("Bad word found", message.Message);
+                },
+                message =>
+                {
+                    Assert.Equal(moderator, message.Sender);
+                    Assert.StartsWith("Bad word found", message.Message);
+                }
+            );
+
+        }
+
+        private class ChatModerator : Participant, IParticipant, IVoidHandler<SendChatMessage.Command>
+        {
+            private readonly IEnumerable<string> _badWords;
+            private readonly IMediator _mediator;
+            private readonly IChatRoom _moderatorChatRoom;
+
+            public ChatModerator(IMediator mediator, IChatRoom moderatorChatRoom, IEnumerable<string> badWords)
+                : base(
+                    mediator: mediator,
+                    name: nameof(ChatModerator),
+                    messageWriter: new EmptyMessageWriter()
+                )
+            {
+                _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+                _moderatorChatRoom = moderatorChatRoom ?? throw new ArgumentNullException(nameof(moderatorChatRoom));
+                _badWords = badWords ?? throw new ArgumentNullException(nameof(badWords));
+            }
+
+            public void Handle(SendChatMessage.Command command)
+            {
+                if(command.Message.Sender == this || command.ChatRoom == _moderatorChatRoom)
+                {
+                    return;
+                }
+
+                var containsAnyBadWord = _badWords.Any(x => command.Message.Message.Contains(x));
+                if (containsAnyBadWord)
+                {
+                    var message = $"Bad word found in message sent by '{command.Message.Sender.Name}' on '{command.Message.Date}' in room '{command.ChatRoom.Name}'\n> {command.Message.Message}"; 
+                    _mediator.Send(new SendChatMessage.Command(
+                        _moderatorChatRoom,
+                        new ChatMessage(this, message)
+                    ));
+                }
+            }
+
+            private class EmptyMessageWriter : IMessageWriter
+            {
+                public void Write(IChatRoom chatRoom, ChatMessage message) { }
+            }
+        }
     }
 
     public class TestMessageWriter : IMessageWriter
