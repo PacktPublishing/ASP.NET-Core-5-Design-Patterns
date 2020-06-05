debeee424370faa0247d2b405263ed3421dadf49
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-06-05 12:48:02 -0400

    Skip test that are expected to fail in the CI build

--- C03/Liskov Substitution/LSP.Tests/Models/HallOfHeroes2Test.cs
+++ C03/Liskov Substitution/LSP.Tests/Models/HallOfHeroes2Test.cs
@@ -1,7 +1,15 @@
-﻿namespace LSP.Examples.Update2
+﻿using Xunit;
+
+namespace LSP.Examples.Update2
 {
     public class HallOfHeroesTest : BaseLSPTest
     {
         protected override HallOfFame sut { get; } = new HallOfHeroes();
+
+        [Trait("FailureExpected", "true")]
+        public override void Add_should_not_add_existing_ninja()
+        {
+            base.Add_should_not_add_existing_ninja();
+        }
     }
 }
