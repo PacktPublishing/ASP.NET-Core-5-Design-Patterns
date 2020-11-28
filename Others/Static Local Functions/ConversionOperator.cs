f9f1e84bbfc026d1f4c4ef411e511f1d4c086557
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-11-27 19:02:09 -0500

    Update ConversionOperator test

--- Others/Static Local Functions/ConversionOperator.cs
+++ Others/Static Local Functions/ConversionOperator.cs
@@ -8,10 +8,21 @@ namespace ConversionOperator
         [Fact]
         public void Value_should_be_set_implicitly()
         {
-            var result = GetValue("Test");
+            var value = "Test";
+            SomeGenericClass<string> result = value;
             Assert.Equal("Test", result.Value);
+        }
 
-            static SomeGenericClass<string> GetValue(string value)
+        [Fact]
+        public void Value_should_be_set_implicitly_using_local_function()
+        {
+            var result1 = GetValue("Test");
+            Assert.Equal("Test", result1.Value);
+
+            var result2 = GetValue(123);
+            Assert.Equal(123, result2.Value);
+
+            static SomeGenericClass<T> GetValue<T>(T value)
             {
                 return value;
             }
