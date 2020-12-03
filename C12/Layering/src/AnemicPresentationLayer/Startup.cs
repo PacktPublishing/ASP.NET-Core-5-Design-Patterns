c1389cc3716e35e5807ddff905580e4c1ab4907f
Carl-Hugo Marcotte <carlhugo@gmail.com>
2020-12-03 11:09:13 -0500

    Rename root namespace

--- C12/Layering/src/AnemicPresentationLayer/Startup.cs
+++ C12/Layering/src/AnemicPresentationLayer/Startup.cs
@@ -16,7 +16,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace RichPresentationLayer
+namespace AnemicPresentationLayer
 {
     public class Startup
     {
